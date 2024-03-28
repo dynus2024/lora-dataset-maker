5e99621b27742ecd1e58095c4f8ceb85cae90481
dynus2024 <dynus2024@gmail.com>
2024-03-29 00:41:48 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { initFlowbite } from 'flowbite';
+
+onMounted(() => {
+  initFlowbite();
+})
+</script>
+
+<template>
+  <div class="bg-slate-200 rounded font-bold font-serif p-4 m-4">Hello</div>
+</template>
